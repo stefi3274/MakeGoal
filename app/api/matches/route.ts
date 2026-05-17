d6fab4ec50475f7fcadd7939310f31f9519fd5d8
unknown <stefi3274@gmail.com>
2026-05-18 01:44:18 +0200

    fix matches route

--- app/api/matches/route.ts
+++ app/api/matches/route.ts
@@ -1,8 +1,3 @@
 export async function GET() {
-  const response = await fetch(
-    'https://www.thesportsdb.com/api/v1/json/123/eventsnextleague.php?id=4334'
-  );
-
-  const data = await response.json();
-  return Response.json(data);
+  return Response.json({ matches: [] });
 }
