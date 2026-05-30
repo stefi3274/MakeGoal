8c82f8e3502db4cb0ded1d8239b2ede2f23f298d
unknown <stefi3274@gmail.com>
2026-05-30 03:05:07 +0200

    fix stats imports

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -1,8 +1,7 @@
 'use client';
 import { useEffect, useState } from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 type Odds = {
   home: number | null;
   draw: number | null;
